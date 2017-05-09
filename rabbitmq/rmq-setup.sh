db80cfae69ac6ed47421eacb573cd7e9d3b3cfba
dukerspace <montolsaklor@gmail.com>
2017-05-09 16:17:45 +0700

    Update rmq-setup.sh

--- rabbitmq/rmq-setup.sh
+++ rabbitmq/rmq-setup.sh
@@ -5,5 +5,9 @@ superuser=guest
 superpass=guest
 vh=tradconnect
 server=localhost
-$rma declare user --user=$superuser --password=$superpass --host=$server name=tctmkdfsubuser password=123456 tags=tradconnect_service
-$rma declare permission --user=$superuser --password=$superpass vhost=$vh --host=$server user=tctmkdfsubuser configure=.* read=.* write=.*
+supuser=subuser
+suppass=123456
+tags=service
+
+$rma declare user --user=$superuser --password=$superpass --host=$server name=$supuser password=$suppass tags=$tags
+$rma declare permission --user=$superuser --password=$superpass vhost=$vh --host=$server user=$supuser configure=.* read=.* write=.*
